91c275824d7fee26049d0e85a67dcaba8b716d1f
FionaNatasha <limanskoe1-3@mail.ru>
2019-11-23 16:44:40 +0300

    chat

--- src/UI/components/Messager/ChatsWindow/FormSend/FormSend.jsx
+++ src/UI/components/Messager/ChatsWindow/FormSend/FormSend.jsx
@@ -1,17 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-export class FormSend extends React.Component {
-    render() {
-        return (
-            <>
-                {/*<div onSubmit={props.handleSubmit} className={`${css.SendMessages} clearFix`}>*/}
-                {/*    <textarea className={`${css.inputMessages}`} name={"newMessage"}*/}
-                {/*           rows={2} placeholder={'Enter Messages Text...'}/>*/}
-                {/*    <button className={css.buttonSendMessages}>Send</button>*/}
-                {/*</div>*/}
-            </>
-=======
 import css from './FormSend.module.css';
 
 export class FormSend extends React.Component {
@@ -23,7 +10,6 @@ export class FormSend extends React.Component {
                           placeholder={'Enter Messages Text...'}/>
                 <button className={css.buttonSendMessages}>Send</button>
             </div>
->>>>>>> 0f6ca578eaeff5defc062a86fe54a92619cda898
         )
     }
 }
--- src/UI/components/Messager/ChatsWindow/MessagesWindow/MessagesWindow.jsx
+++ src/UI/components/Messager/ChatsWindow/MessagesWindow/MessagesWindow.jsx
@@ -4,11 +4,6 @@ export class MessagesWindow extends React.Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
-                MessagesWindow
-=======
-
->>>>>>> 0f6ca578eaeff5defc062a86fe54a92619cda898
             </div>
         )
     }
