15d61ae316ca8c1183afb6a190fed513ea3db82e
itemka <itemka2503@gmail.com>
2019-11-23 18:33:39 +0300

    testCommit

--- src/UI/components/Messager/ChatsWindow/FormSend/FormSend.jsx
+++ src/UI/components/Messager/ChatsWindow/FormSend/FormSend.jsx
@@ -2,13 +2,31 @@ import React from 'react';
 import css from './FormSend.module.css';
 
 export class FormSend extends React.Component {
+    state = {
+        areaText: '',
+    };
+
+    addNewTextToTextarea = (e) => {
+        this.setState({areaText: e.currentTarget.value})
+    };
+
+    onKeyPress = (e) => {
+        if (e.key === 'Enter') this.props.addNewMessage(this.state.areaText)
+    };
 
     render() {
         return (
             <div className={`${css.FormSend} clearFix`}>
-                <textarea className={`${css.inputMessages}`} name={"newMessage"} rows={2}
-                          placeholder={'Enter Messages Text...'}/>
-                <button className={css.buttonSendMessages} onClick={()=>this.props.addNewMessage(`fdsdgs`)}>Send</button>
+                <textarea className={`${css.inputMessages}`}
+                          name={"newMessage"}
+                          onChange={this.addNewTextToTextarea}
+                          rows={2}
+                          value={this.state.areaText}
+                          placeholder={'Enter Messages Text...'}
+                          onKeyPress={this.onKeyPress}/>
+                <button className={css.buttonSendMessages}
+                        onClick={() => this.props.addNewMessage(this.state.areaText)}>Send
+                </button>
             </div>
         )
     }
